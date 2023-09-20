7aa4a3407fc2a7bf949bdfd604a3a947fcfb421c
El-hlawany <aboabdallahhlawany@gmail.com>
2023-09-20 13:24:02 -0700

    first c file has a first c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * this is a first c code in this project
+ * it is a simple code for print some text 
+ * on the console desplay
+ */
+
+int main()
+{
+  puts("programming is like building a multilingual puzzle");
+  return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -2,3 +2,4 @@ this is a first task for write script that read a c file in preprocessor format
 compile a c file but stop before make liker
 convert c file to symble file
 the convert c to excutable and name it
+4 this is a first c code
