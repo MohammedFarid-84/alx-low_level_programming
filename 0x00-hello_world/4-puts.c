d7c00754041a92cf62afa4153f85682176a4e334
El-hlawany <aboabdallahhlawany@gmail.com>
2023-09-21 02:33:20 -0700

    edit c code file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 /**
-* this is a first c code in this project
-* it is a simple code for print some text 
-* on the console desplay
-*/
-
+ * this is a first c code in this project
+ * it is a simple code for print some text 
+ * on the console desplay
+ */
 int main(void)
 {
   /**
-   * the puts is a biltin function on c 
-   * it show a message on scrin like printf
-   */
-
-  puts ("\"programming is like building a multilingual puzzle");
-  
+  * the puts is a biltin function on c 
+  * it show a message on scrin like printf
+  */
+  puts("\"programming is like building a multilingual puzzle"); 
   /** return main function alwayse scusses */
   return (0);
 }
