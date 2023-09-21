5cf3c4a605c541c63c23e40aa87abfd669ebd837
El-hlawany <aboabdallahhlawany@gmail.com>
2023-09-21 05:44:10 -0700

    use printf function in c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - printf "text"
+ *
+ * void: there's no thing here
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
