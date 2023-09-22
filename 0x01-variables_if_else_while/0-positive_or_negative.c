27216a67fb5d83247750b75f443b18bcb2822eb7
El-hlawany <aboabdallahhlawany@gmail.com>
2023-09-22 14:04:40 -0700

    choise a random number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * Main - if else statment and printf
+ * Return: 0 always.
+ */
+
+int main(void)
+{
+	int n;
+	char *dsc;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	/* your code goes there */
+
+	if(n < 0){
+		dsc = " is negative";
+	}else if(n == 0) {
+		dsc = " is zero";
+	}else{ dsc = " is positive";
+	}
+
+	printf("%d%s\n", n, dsc);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+this project is about if_while func in c\n--------------------
+0- file has a c code for chois a random numbers
