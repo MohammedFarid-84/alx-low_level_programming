56c03a4adbda6edee6635d95b1c280784946c25c
El-hlawany <aboabdallahhlawany@gmail.com>
2023-09-25 22:05:45 -0700

    print text with my defintion function

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* this is a header file inclue the nessery libraries and prototype functions */
+
+/*here include the nessery libraries */
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/*here define the prototype functions*/
+int _putchar(char c);
+
+
+/*ending header*/
+#endif
