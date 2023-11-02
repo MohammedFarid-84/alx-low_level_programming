d5d61afd17128a8ef68750c36ef28f80b78bdaa9
El-Hlawany <aboabdallahhlawany@gmial.com>
2023-11-02 22:10:24 +0200

    edit print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * print alphabet charecters
+ * print_alphabet -  prints alphabet in lawercase.
  *
  * Return: Always 0.
  */
 
 int print_alphabet(void)
 {
+	/* set char a as integer in i varibale */
 	char i = 'a';
+
 	while (i <= 'z')
 	{
 		_putchar(i);
