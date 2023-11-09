0fe57a935806a95f1cb45eaa93cb501d662e7f4c
El-hlawany <aboabdallahhlawany@gmail.com>
2023-11-09 20:21:14 +0200

    find a remaining of days in year

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - takes a date and prints how many days are left in the year, taking
+ * leap years into account
+ * Return: 0
+ */
+
+int main(void)
+{
+	int month;
+	int day;
+	int year;
+
+	printf("%s\n", "please insert a month number between 1 to 12: ");
+	scanf("%d", &month);
+
+	printf("%s\n", "please insert a day of month between 1 to 31: ");
+	scanf("%d", &day);
+
+	printf("%s\n", "please insert a year number: ");
+	scanf("%d", &year);
+
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+
+	return (0);
+}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,6 +7,7 @@
 /* define the prototype functions */
 void positive_or_negative(int i);
 int largest_number(int, int, int);
-
+int convert_day(int, int);
+void print_remaining_days(int month, int day, int year);
 
 #endif 
