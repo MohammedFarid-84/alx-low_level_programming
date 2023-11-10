e3b7e6dcd328cbe9e6d621105115cd93cb7d6802
El-hlawany <aboabdallahhlawany@gmail.com>
2023-11-10 10:31:07 +0200

    print numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,13 +4,16 @@
 /** THE NESSERY LIBRARIES **/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 
 /** THE PROTOTYPE FUNCTIONS **/
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int, int);
-
+void print_numbers(void);
+int _putchar(char c);
 
 
 
