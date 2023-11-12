408696c9f8fe438ff31200a166047356d2bca921
El-hlawany <aboabdallahhlawany@gmail.com>
2023-11-12 21:38:04 +0200

    print large prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,29 +10,31 @@ int main(void)
 {
 	long int i = 612852475143;
 	long x = 2;
-	int pf[100], y, z, max;
+	int pf[100], y = 0, z, max;
 
-	for (; i > 0; i /= x)
+	while (i > 1)
 	{
 		if (i % x == 0)
 		{
-			y += 1;
-			/* printf("prime factors members is: %lu\n", x); */
+			i /= x;
 			pf[y] = x;
-		}
-
-		if (x == 2)
-		{
-			x += 1;
+			y += 1;
 		}
 		else
 		{
-			x += 2;
+			if (x == 2)
+			{
+				x += 1;
+			}
+			else
+			{
+				x += 2;
+			}
 		}
 	}
 
-	z = sizeof(pf) / sizeof(pf[0]);
-	max = pf[0];
+	z = y;
+	max = 2;
 
 	for (y = 0; y < z; y++)
 	{
@@ -40,9 +42,8 @@ int main(void)
 		{
 			max = pf[y];
 		}
-		/* printf("mem %d - (%d)", y, pf[y]); */
 	}
-	/* printf("this is a large prime factor number: %d\n of number %ld\n", max, i); */
+
 	printf("%d\n", max);
 
 	return (0);
