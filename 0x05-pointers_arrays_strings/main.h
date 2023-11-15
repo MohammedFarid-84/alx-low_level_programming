21d13736c2c60ef7a0b3238a965c7c8db37fb8f1
El-hlawany <aboabdallahhlawany@gmail.com>
2023-11-15 07:56:26 +0200

    change pointer in func

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * my standard library main.h 
+ * include nessary bilut-in library and prototype functions.
+ */
+
+/** standrad library **/
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <stdlib.h>
+
+/** prototype functions **/
+void reset_to_98(int *n);
+
+
+
+
+
+#endif
