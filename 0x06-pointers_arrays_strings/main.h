9b99cedcb6ed324b0a70c775cd7f74e6126b3091
El-hlawany <aboabdallahhlawany@gmail.com>
2023-11-17 23:53:32 +0200

    concated two string

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/** nessesry libraries **/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
+
+/** prototype functions **/
+char *_strcat(char *dest, char *src);
+
+
+
+
+
+#endif
