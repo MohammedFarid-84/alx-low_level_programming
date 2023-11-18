5e2f89e84881ff3200cc08db0ead7a3e1d4d63a9
El-hlawany <aboabdallahhlawany@gmail.com>
2023-11-18 14:00:40 +0200

    compare a two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare a two strings are same or not.
+ * @s1: a first string.
+ * @s2: a second string.
+ * Return: 0 if same and less than and greter than.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	/** int x = strlen(s1); **/
+	/** int y = strlen(s2); **/
+	int i = 0;
+
+	i = s1[0] - s2[0];
+
+	return (i);
+}
