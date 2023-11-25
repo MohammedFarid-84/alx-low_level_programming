9aa3e68bdfb0b5dc6cf71cbaea191e6fb9e8c28d
El-hlawany <aboabdallahhlawany@gmail.com>
2023-11-25 13:11:27 +0200

    search for substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,8 +11,12 @@ unsigned int _strspn(char *s, char *f)
 {
 	/* int halflens = strlen(s) / 2; */
 	int i = 0;
-
-	if (strchr(f, s[0]) != 0)
+	
+	if (strlen(s) == 0 || strlen(f) == 0)
+	{
+		return (i);
+	}
+	else if (strchr(f, s[0]) != 0)
 	{
 		while (strchr(s, f[i]) == 0)
 			i++;
