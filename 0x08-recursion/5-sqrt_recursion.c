ab359058408155bc35bd8cdcb70e51af23b72d06
El-hlawany <aboabdallahhlawany@gmail.com>
2023-12-11 08:05:15 +0200

    answer task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,31 @@
 #include "main.h"
 
 /**
+* _sqrt - calc a squar root.
+* @n: a number to get a squar for it.
+* @i: an increment value from 0 to n number.
+* Return: the result of squar root.
+*/
+
+int _sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+
+	if (i * i == n)
+		return (i);
+
+	return (_sqrt(n, i + 1));
+}
+
+/**
 * _sqrt_recursion - calc a squar root of number x.
-* @x: a number for calc.
+* @n: a number for calc.
 * Return: a resulte.
 */
 
 int _sqrt_recursion(int n)
 {
-	static int i = 0;
-
 	if (n < 0)
 		return (-1);
 
@@ -19,19 +35,5 @@ int _sqrt_recursion(int n)
 	if (n == 1)
 		return (1);
 
-	if (i * i > n)
-		return (-1);
-
-	if (i * i == n)
-	{
-		int x = i;
-		i = 0;
-		return (x);
-	}
-	else
-	{
-		++i;
-		return (_sqrt_recursion(n));
-	}
-	return (-1);
+	return (_sqrt(n, 0));
 }
