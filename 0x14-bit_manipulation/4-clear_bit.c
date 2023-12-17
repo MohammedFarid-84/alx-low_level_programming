6ade781a22a85ca674ab96f87b1355b9f7b80dac
El-hlawany <aboabdallahhlawany@gmail.com>
2023-12-17 02:53:13 +0200

    answer task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - convert bits from an index into zero
+ * @n: a number.
+ * @index: an index.
+ * Return: 1 if true and -1 if false.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int x = ((sizeof(*n) * 8) - 1);
+
+	if (index > x )
+		return (-1);
+
+	*n =  *n & ~(1 << index);
+	return (1);
+}
