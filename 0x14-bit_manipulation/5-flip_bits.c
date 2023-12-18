b285a3e72f215d8773b7c0efdb18d5912ffef0f6
El-hlawany <aboabdallahhlawany@gmail.com>
2023-12-18 06:27:22 +0200

    answer task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * flip_bits - git a count of bits if changed
+ * convert number to another number.
+ * @n: a first number.
+ * @m: a second number.
+ * Return: count of deferent bits.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int bn = n;
+	unsigned long int bm = m;
+	unsigned int i = 0;
+	unsigned int len = ((sizeof(n) * 8) - 1);
+	int count = 0;
+
+	while (i < len)
+	{
+		if ((bn >> i & 1) != (bm >> i & 1))
+			count++;
+		i++;
+	}
+	return (count);
+}
