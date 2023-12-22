0fcb6ab9df361d2d2202cc8a89b7ee1a893ddcb2
El-hlawany <aboabdallahhlawany@gmail.com>
2023-12-22 13:41:45 +0200

    update some files and load a new directory for new project.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,25 +9,25 @@
 int main(void)
 {
 	int i = 1;
-	long int n1, n2, n3;
+	unsigned long long int n1, n2, n3;
 
 	n1 = 1;
 	n2 = 2;
 	n3 = 0;
 
-	printf("%ld, %ld, ", n1, n2);
+	printf("%llu, %llu, ", n1, n2);
 
 	for (i = 3; i <= 98; i++)
 	{
-		n3 = n1 + n2;
+		n3 = (n1 + n2);
 
 		if (i == 98)
 		{
-			printf("%ld\n", n3);
+			printf("%llu\n", n3);
 		}
 		else
 		{
-			printf("%ld, ", n3);
+			printf("%llu, ", n3);
 		}
 		n1 = n2;
 		n2 = n3;
