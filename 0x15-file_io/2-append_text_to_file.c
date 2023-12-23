2d5212ec2c0ad341763592fd9b996eea7f0bee1f
El-hlawany <aboabdallahhlawany@gmail.com>
2023-12-23 08:04:30 +0200

    update create text file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,26 +1,23 @@
 #include "main.h"
 
 /**
- * create_file - create a file with certain data if it not excest
+ * append_text_to_file - create a file with certain data if it not excest
  * and edit it if it exsest with read only permasson.
  * @filename: a file name.
  * @text_content: a text will write into file.
  * Return: 1 if no errors and -1 if fail.
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fl = 0;
 
-	if (strlen(filename) == 0)
-		return (-1);
-
-	fl = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fl = open(filename, O_APPEND, 0600);
 
 	if (!fl)
 		return (-1);
 
-	if (write(fl, text_content, strlen(text_content)) == -1)
+	if (write(fl, text_content, sizeof(text_content)) == -1)
 		return (-1);
 
 	close(fl);
