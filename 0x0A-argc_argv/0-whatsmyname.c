7cea90cd0513760939d1b27e5fc85cb650f31a1a
El-hlawany <aboabdallahhlawany@gmail.com>
2024-01-26 17:08:57 +0200

    show my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - show the program name.
+ * @argc: is count of arguments.
+ * @argv: is arguments of program.
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
