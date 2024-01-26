7e5c01dfa40c8797019c7cb0ac97eef294db91ae
El-hlawany <aboabdallahhlawany@gmail.com>
2024-01-26 19:41:53 +0200

    print all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,14 +7,15 @@
  * Return: zero always.
  */
 
- int main(int argc, char **argv)
- {
-    int i = 0;
+int main(int argc, char **argv)
+{
+	int i = 0;
 
-    while i <= argc
-    {
-        printf("%s\n", argv[i]);
-    }
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
 
-    return (0);
- }
+	return (0);
+}
