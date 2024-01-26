045391b56021c0d9d0669be40fd6da5e31fa4d34
El-hlawany <aboabdallahhlawany@gmail.com>
2024-01-26 17:29:30 +0200

    show count of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - show count of arguments on command line.
+ * @argc: count of arguments.
+ * @argv: an array of arguments.
+ * Return: zero always.
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
