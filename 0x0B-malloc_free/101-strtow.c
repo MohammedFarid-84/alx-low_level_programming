bdfb7ddff9df1d7555dc8a7954b9fbfe35cf23eb
El-hlawany <aboabdallahhlawany@gmail.com>
2024-02-04 15:09:28 +0200

    split words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,107 @@
+#include "main.h"
+
+/**
+ * lenstr - calculate length of a string.
+ * @strs: a string.
+ * Return: count of charecters or 0 if null.
+ */
+unsigned int lenstr(char *strs)
+{
+	unsigned int i = 0;
+
+	if (strs == NULL)
+		return (0);
+
+	while (*strs != '\0')
+	{
+		i++;
+		strs++;
+	}
+	return (i);
+}
+
+/**
+ * free_ary - free allocated memory.
+ * @ary: a pointer to a memory.
+ * @n: count of allocated paces.
+ * Return: void.
+ */
+void free_ary(char **ary, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		free(ary[i]);
+		i++;
+	}
+	free(ary);
+}
+
+/**
+ * splitwords - split a string by words.
+ * @str: a string.
+ * Return: array of words or null.
+ */
+char **splitwords(char *str)
+{
+	char **rwords, **words;
+	int w = 0, cr = 0, i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	rwords = malloc(sizeof(char *) * lenstr(str));
+	if (rwords == NULL)
+		return (NULL);
+	while (*str != '\0')
+	{
+		if (rwords[w] == NULL)
+			rwords[w] = malloc(sizeof(char) * 20);
+		if (*str != ' ')
+		{
+			if (rwords[w] == NULL)
+			{
+				free_ary(rwords, w);
+				return (NULL);
+			}
+			rwords[w][cr] = *str;
+			cr++;
+		}
+		else
+		{
+			rwords[w][cr] = '\0';
+			cr = 0;
+			w++;
+		}
+		str++;
+	}
+	rwords[w] = NULL;
+	words = malloc(sizeof(char *) * w);
+	w = 0;
+	while (rwords[w] != NULL)
+	{
+		if (lenstr(rwords[w]) != 0)
+		{
+			words[i] = rwords[w];
+			i++;
+		}
+		w++;
+	}
+	words[i] = NULL;
+	return (words);
+}
+
+/**
+ * strtow - split text into words.
+ * @str: a text.
+ * Return: a pointer to text.
+ */
+char **strtow(char *str)
+{
+	char **newstr;
+
+	newstr = splitwords(str);
+	if (newstr != NULL)
+		return (newstr);
+	return (NULL);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -18,5 +18,7 @@ char *str_concat(char *, char *);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
 
 #endif
