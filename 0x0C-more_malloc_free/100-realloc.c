e8008fc42ec1e9006f4fc04bb246884b6443dbe6
El-hlawany <aboabdallahhlawany@gmail.com>
2024-02-10 00:51:09 +0200

    edit realloc again

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,10 +13,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *nptr;
 
 	if (new_size == 0)
+	{
 		if (ptr != NULL)
 			free(ptr);
 		else
 			return (NULL);
+	}
 
 	if (ptr == NULL)
 	{
