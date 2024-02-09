aa6483d983ad45ac9f259c67e6a1087e8d0caf91
El-hlawany <aboabdallahhlawany@gmail.com>
2024-02-10 00:29:56 +0200

    new realloc

--- 0x0C-more_malloc_free/100-main.c
+++ 0x0C-more_malloc_free/100-main.c
@@ -35,16 +35,22 @@ void simple_print_buffer(char *buffer, unsigned int size)
 int main(void)
 {
 	char *p;
-	int i;
+	unsigned int i;
+	unsigned int new_size;
 
-	/* p = malloc(sizeof(char) * 30); */
-	p = _realloc(NULL, sizeof(char) * 30, sizeof(char) * 120);
+	new_size = 0;
+	p = _realloc(NULL, sizeof(char) * 30, sizeof(char) * new_size);
+	if (p == NULL)
+	{
+		printf("Failed\n");
+		return (1);
+	}
 	i = 0;
-	while (i < 98)
+	while (i < new_size)
 	{
 		p[i++] = 98;
 	}
-	simple_print_buffer(p, 98);
+	simple_print_buffer(p, new_size);
 	free(p);
 	return (0);
 }
