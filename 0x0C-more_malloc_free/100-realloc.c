442d94e6c6999bb225c3f69c4d518433798c6b72
El-hlawany <aboabdallahhlawany@gmail.com>
2024-02-09 23:54:05 +0200

    realloc func

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** _realloc - re-allocats memory.
+/**
+ * _realloc - re-allocats memory.
  * @ptr: an old memory allocated.
  * @old_size: an old size of allocated memory.
  * @new_size:: a new size of allocated memory.
@@ -14,9 +15,19 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 		nptr = malloc(new_size);
 	if (new_size == 0)
+	{
+		free(ptr);
 		return (NULL);
-	if (old_size == new_size)
-		return (ptr);
+	}
+	else if (old_size == new_size)
+	{
+		nptr = ptr;
+	}
+	else if (old_size != new_size)
+	{
+		nptr = malloc(new_size);
+		memcpy(nptr, ptr, old_size);
+		free(ptr);
+	}
 	return (nptr);
 }
-
