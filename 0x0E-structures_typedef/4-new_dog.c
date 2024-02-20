9ed5b1ab94e672a5ef205533e2923d9ca84e1955
El-hlawany <aboabdallahhlawany@gmail.com>
2024-02-20 23:31:38 +0200

    edit struct with func

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,17 +10,28 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p = malloc((sizeof(char *) * 2) + 8);
+	dog_t *p = malloc(sizeof(dog_t));
 
 	if (p == NULL)
+		return (NULL);
+
+	p->name = malloc(strlen(name) + 1);
+	if (p->name == NULL)
 	{
 		free(p);
 		return (NULL);
 	}
+	strcpy(p->name, name);
 
-	p->name = name;
 	p->age = age;
-	p->owner = owner;
+
+	p->owner = malloc(strlen(owner) + 1);
+	if (p->owner == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	strcpy(p->owner, owner);
 
 	return (p);
 }
