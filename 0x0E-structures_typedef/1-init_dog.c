aad13dd489a93ad31333ad9243afb35a3f0458bc
El-hlawany <aboabdallahhlawany@gmail.com>
2024-02-20 20:12:08 +0200

    edit init elements of sturcture

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,11 +5,14 @@
  * @d: is a pointer to struct it will init.
  * @name: is first member of struct.
  * @age: is a second member of struct.
- * @ owner: is a last member of struct.
+ * @owner: is a last member of struct.
  * Return: void.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+
+	if (d == NULL)
+		return;
 
 	d->name = name;
 	d->age = age;
