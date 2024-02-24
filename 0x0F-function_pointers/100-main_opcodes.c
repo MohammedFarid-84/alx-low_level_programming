9a3de560de06115643f3cb0718395188a27259d2
El-hlawany <aboabdallahhlawany@gmail.com>
2024-02-24 22:17:53 +0200

    read excutable file

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print opcode with a spacefic format.
+ * @argc: arguments count.
+ * @argv: arguments.
+ * Return: zero always.
+ */
+int main(int argc, char **argv)
+{
+	int x = 0;
+	FILE *f;
+	size_t rdbytf = 0, i = 0;
+	unsigned char bfr[1024];
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	x = atoi(argv[1]);
+	if (x <= 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	f = fopen(argv[0], "rb");
+	if (f == NULL)
+		return (0);
+	rdbytf = fread(bfr, sizeof(unsigned char), x, f);
+	for (i = 0; i < rdbytf; i++)
+	{
+		printf("%02x ", bfr[i]);
+	}
+	printf("\n");
+	fclose(f);
+	return (0);
+}
