7ac1dc7a8b0f8c4c5c38bdf8e646edd0a0e32496
El-hlawany <aboabdallahhlawany@gmail.com>
2024-02-25 20:03:41 +0200

    print opcode

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 {
 	int x = 0;
 	FILE *f;
-	size_t rdbytf = 0, i = 0;
+	size_t rdbytf = 0, i = 0, siz = 0;
 	unsigned char bfr[1024];
 
 	if (argc != 2)
@@ -23,11 +23,17 @@ int main(int argc, char **argv)
 	if (x <= 0)
 	{
 		printf("Error\n");
-		exit(1);
+		exit(2);
 	}
 	f = fopen(argv[0], "rb");
 	if (f == NULL)
 		return (0);
+	fseek(f, 0, SEEK_END);
+	siz = ftell(f);
+	fseek(f, 0, SEEK_SET);
+	if (siz  < x)
+		exit(1);
+
 	rdbytf = fread(bfr, sizeof(unsigned char), x, f);
 	for (i = 0; i < rdbytf; i++)
 	{
