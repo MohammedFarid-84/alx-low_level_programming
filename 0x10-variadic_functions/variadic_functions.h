415077a40304ce33c2fc63ac4c093089a5e1847d
El-hlawany <aboabdallahhlawany@gmail.com>
2024-02-25 23:19:42 +0200

    upload variadic function project

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,20 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/* NECESSARY LIBRARIES */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+
+
+
+/* MY FUNCTIONS PROTOTYPES */
+int _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+
+
+
+
+#endif
