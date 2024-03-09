d474b5def50023ea593f018a05491cf33c86b0c7
El-hlawany <aboabdallahhlawany@gmail.com>
2024-03-09 21:33:13 +0200

    add more func in lybrary

--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -7,6 +7,9 @@
  */
 int main(void)
 {
-	printf("%d\n", _strlen("My Dyn Lib"));
+	printf("test strlen of 'My Dyn Lib is:%d\n", _strlen("My Dyn Lib"));
+	printf("test abs of -8 func is:%d\n", _abs(-8));
+	printf("test atoi of 1 is %d\n", _atoi("1"));
+
 	return (EXIT_SUCCESS);
 }
