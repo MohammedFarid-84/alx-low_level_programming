56751446254c701efb9b388570471bc2f1a0ac7d
Mohamed Farid <elhlawany@DESKTOP-PQ5SIJS.localdomain>
2024-03-15 18:22:58 +0200

    print before lunnch

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_func - print a message before lunch a programm.
+ *
+ * Return: void.
+ */
+
+void __attribute__((constructor)) print_func(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
