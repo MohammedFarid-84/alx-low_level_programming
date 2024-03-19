e444f70376143467492f3662e0032c510168307f
Mohamed Farid <aboabdallahhlawany@gmail.com>
2024-03-19 03:21:07 +0200

    set list to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint - free allocated memory to list.
+ * @head: a list will free.
+ *
+ * Return: void.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+	listint_t *hd = *head;
+
+	if (head == NULL)
+		return;
+
+	while (hd != NULL)
+	{
+		tmp = hd->next;
+		free(hd);
+		hd = tmp;
+	}
+	hd = NULL;
+}
+
